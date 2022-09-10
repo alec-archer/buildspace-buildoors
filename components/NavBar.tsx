927e82f61cdafe08fd011729fdae7fd6513aa0ed
Alec Archer <alec.archer@tutanota.com>
2022-09-09 18:38:24 -0700

    Complete section 1.4

---
+++ components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { HStack, Spacer } from "@chakra-ui/react";
+import {WalletMultiButton} from '@solana/wallet-adapter-react-ui'
+
+const NavBar: FC = () => {
+    return (
+        <HStack width="full" padding={4}>
+            <Spacer />
+            <WalletMultiButton></WalletMultiButton>
+        </HStack>
+    )
+}
+
+export default NavBar
