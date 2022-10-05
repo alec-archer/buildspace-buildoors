38c040d2d3e5c41fb96be0f10643e76e0a8a89ac
Alec Archer <alec.archer@tutanota.com>
2022-10-05 16:46:10 -0700

    Migrate to anchor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import WalletContextProvider from '../components/WalletContextProvider'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import WalletContextProvider from "../components/WalletContextProvider";
+import { WorkspaceProvider } from "../components/WorkspaceProvider";
 
 const colors = {
   background: "1F1F1F",
   accent: "#833BBE",
-  bodyText: "rgba(255,255,255,0.75)"
-}
+  bodyText: "rgba(255,255,255,0.75)",
+};
 
-const theme = extendTheme({colors})
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <WalletContextProvider>
-      <Component {...pageProps} />
-</WalletContextProvider>
-    </ChakraProvider>)
+        <WorkspaceProvider>
+          <Component {...pageProps} />
+        </WorkspaceProvider>
+      </WalletContextProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
